f7c0d2f5bcbb892a61fdedce269dcb21b878341b
A712\Alum <manuelpastormartinezz@gmail.com>
2026-03-03 17:20:56 +0100

    Pilas

---
+++ 2oTrimestre/src/main/java/colecciones/Ejercicio1.java
@@ -0,0 +1,24 @@
+package colecciones;
+
+import java.util.Stack;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Stack<String>letras = new Stack<>();
+
+        letras.push("a");
+        letras.push("b");
+        letras.push("c");
+        letras.push("d");
+        letras.push("e");
+
+        System.out.println(letras.isEmpty());
+        System.out.println(letras.size());
+        System.out.println(letras.peek());
+        for (String letra :letras){
+            if (letra.equals(letra)){
+                System.out.println(letras.pop());
+            }
+        }
+    }
+}
