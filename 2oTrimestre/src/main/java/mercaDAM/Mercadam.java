64609f1666f5652dc977ab481e4d7d4eaa694091
A712\Alum <manuelpastormartinezz@gmail.com>
2026-03-20 20:34:14 +0200

    Haciendo la practica

--- 2oTrimestre/src/main/java/mercaDAM/Mercadam.java
+++ 2oTrimestre/src/main/java/mercaDAM/Mercadam.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Collections;
+import java.util.Random;
 import java.util.Set;
 
 @Getter
 @Setter
 public class Mercadam {
+    static String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    static Random random = new Random();
     private Set<Cliente> clientes;
 
     public Mercadam(){
@@ -16,7 +19,15 @@ public class Mercadam {
     }
 
     public void generarClientes(){
+        String usuario = "";
+        String contrasenya = "";
+        for (int i = 0; i < 8; i++) {
+            usuario += caracteres.charAt(random.nextInt(caracteres.length()-1));
+            contrasenya += caracteres.charAt(random.nextInt(caracteres.length()-1));
+        }
 
+        Cliente cliente = new Cliente(usuario,contrasenya);
+        clientes.add(cliente);
     }
 
     public Set<Cliente> getClientes(Set<Cliente>clientes){
